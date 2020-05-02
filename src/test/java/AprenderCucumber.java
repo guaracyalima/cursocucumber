91e3ce92a9c6768c9d8398fe48cfa5ac8b413155
guaracyalima <guaracyaraujolima@gmail.com>
2020-05-02 05:51:40 -0300

    expressoes regulares

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,6 +1,11 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Então;
@@ -25,6 +30,7 @@ public class AprenderCucumber {
 	}
 
 	private int counter = 0;
+
 	@Dado("que o valor do contador é {int}")
 	public void que_o_valor_do_contador_é(Integer param) {
 		counter = param;
@@ -37,9 +43,75 @@ public class AprenderCucumber {
 
 	@Entao("o valor do contador é {int}")
 	public void o_valor_do_contador_é(Integer param) {
-			
+
 		boolean is = param == counter;
 		assertTrue(is);
-		
+
 	}
+
+	Date date = new Date();
+	@Dado("que a entrga é dia {int}\\/{int}\\/{int}")
+	public void que_a_entrga_é_dia(Integer dia, Integer mes, Integer ano) {
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, dia);
+		calendar.set(Calendar.MONTH, mes - 1);
+		calendar.set(Calendar.YEAR, ano);
+		
+		date = calendar.getTime();
+	}
+
+	@Quando("a entrega atrasar em {int} dias")
+	public void a_entrega_atrasar_em_dias(Integer dias) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.DAY_OF_MONTH, dias);
+		
+		date = calendar.getTime();
+	}
+
+	//@Entao("a entrega será efetuada dia {int}\\/{int}\\/{int}")
+	@Entao("a entrega será efetuada dia (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	//public void a_entrega_será_efetuada_dia(Integer dia, Integer mes, Integer ano) {
+	public void a_entrega_será_efetuada_dia(String _date) {
+		
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dateFormated = format.format(date);
+		boolean verify = dateFormated.equals(_date);
+		
+		assertEquals(dateFormated, _date);
+		assertTrue(verify);
+	}
+	
+	
+	@Dado("que o prazo é {int}\\/{int}\\/{int}")
+	public void que_o_prazo_é(Integer dia, Integer mes, Integer ano) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, dia);
+		calendar.set(Calendar.MONTH, mes - 1);
+		calendar.set(Calendar.YEAR, ano);
+		
+		date = calendar.getTime();
+	}
+
+	@Quando("a entrega atrazar em {int} meses")
+	public void a_entrega_atrazar_em_meses(Integer mes) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.MONTH, mes);
+		
+		date = calendar.getTime();
+	}
+
+	@Entao("a entrega sera efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void a_entrega_sera_efetuada_em(String _date) {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dateFormated = format.format(date);
+		boolean verify = dateFormated.equals(_date);
+		
+		assertEquals(dateFormated, _date);
+		assertTrue(verify);
+	}
+
+
 }
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -15,3 +15,23 @@ Cenario: Deve que incrementar contador
 	Dado que o valor do contador é 15
 	Quando eu incrementar em 3
 	Entao o valor do contador é 18
+	
+Cenario: Deve que incrementar contador
+	Dado que o valor do contador é 20
+	Quando eu incrementar em 3
+	Entao o valor do contador é 23
+	
+Cenario: Deve calcular atraso na entrega
+	Dado que a entrga é dia 05/04/2020
+	Quando a entrega atrasar em 2 dias
+	Entao a entrega será efetuada dia 07/04/2020
+	
+Cenario: Deve calcular atraso no praso de entrega da China
+	Dado que o prazo é 05/05/2020
+	Quando a entrega atrazar em 2 meses
+	Entao a entrega sera efetuada em 05/07/2020
+	
+Cenario: Deve calcular atraso no praso de entrega da China
+	Dado que o prazo é 05/10/2020
+	Quando a entrega atrazar em 2 meses
+	Entao a entrega sera efetuada em 05/12/2020
